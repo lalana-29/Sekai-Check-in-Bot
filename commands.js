3262a5959d3440aaff70c186c6d9f657ee4d520d
Lalana <alexandra.l.davison@gmail.com>
2026-07-09 15:54:02 -0400

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,5 +1,11 @@
 import 'dotenv/config';
 import { InstallGlobalCommands } from './utils.js';
 
-const ALL_COMMANDS = [];
+const TRIGGER_CHECKIN_COMMAND = {
+  name: 'trigger-checkin',
+  description: 'Manually send the current check-in message',
+  type: 1,
+};
+
+const ALL_COMMANDS = [TRIGGER_CHECKIN_COMMAND];
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
