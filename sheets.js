b6e49677255a36dec7b5ef2cd7f8d21ca7c9ee16
Lalana <alexandra.l.davison@gmail.com>
2026-07-09 16:20:32 -0400

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -1,11 +1,10 @@
 import { google } from 'googleapis';
 
-const auth = new google.auth.JWT(
-  process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-  null,
-  process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  ['https://www.googleapis.com/auth/spreadsheets.readonly']
-);
+const auth = new google.auth.JWT({
+  email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+  key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+});
 
 const sheets = google.sheets({ version: 'v4', auth });
 
